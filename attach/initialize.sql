27b32c302b735f151308ad74681ac588ef527df4
k-tanaka <acogig0122@gmail.com>
2013-08-23 22:47:35 +0900

    テーブル作成SQLのファイル名を変更 初期化SQLファイルを追加

---
+++ attach/initialize.sql
@@ -0,0 +1,20 @@
+--
+-- bora-jum Projec
+-- DB Initialize SQL
+-- @author    k-tanaka
+-- @date      2013/08/23
+--
+
+--
+-- Table Name: users
+--
+INSERT INTO users (id,loginid,name,password,type,created_at,updated_at)
+VALUES (
+    1,
+    'system_admin',
+    'ÉVÉXÉeÉÄä«óùé“',
+    'e87b9c4294a316b909102778e008a83b1be49f8895c0215a9cfc888a1644f7442814cb486dc55cddee11fdcb623453ca60c93119f7e689d8dbd86151a4c2e339',
+    0,
+    0,
+    0
+);
