b1215b5a3b880d85358d052ab5795862ded07b0f
k-tanaka <acogig0122@gmail.com>
2013-08-23 22:55:43 +0900

    SQLファイルのエンコーディングをUTF-8/UNIXに統一

--- attach/initialize.sql
+++ attach/initialize.sql
@@ -12,9 +12,9 @@ INSERT INTO users (id,loginid,name,password,type,created_at,updated_at)
 VALUES (
     1,
     'system_admin',
-    'ƒVƒXƒeƒ€ŠÇ—Ò',
+    'ã‚·ã‚¹ãƒ†ãƒ ç®¡ç†è€…',
     'e87b9c4294a316b909102778e008a83b1be49f8895c0215a9cfc888a1644f7442814cb486dc55cddee11fdcb623453ca60c93119f7e689d8dbd86151a4c2e339',
     0,
-    0,
-    0
+    now(),
+    now()
 );
